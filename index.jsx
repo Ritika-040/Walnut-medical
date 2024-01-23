40bbe0e9b8c7823da142519b5836ac88fd7c74ae
Ritika Shama <94158225+Ritika-040@users.noreply.github.com>
2024-01-23 13:30:06 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,108 +1,17 @@
-const iSate = {
-  isLogin: false,
-  user: {
-    token: "",
-    token2: "",
-    f_name: "",
-    l_name: "",
-    role: "",
-    photo: "",
-    employee_id: "",
-  },
-  LineLogin: {
-    line_name: "",
-    line_login_time: "",
-    type: "",
-    isLogedIn: false,
-    line_id: "",
-    active_batch: "",
-  },
+import { configureStore } from "@reduxjs/toolkit";
+import { Reducer } from "../Reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root",
+  storage,
 };
 
-export const Reducer = (state = iSate, action) => {
-  switch (action.type) {
-    case "HANDLE_LOGIN": {
-      console.log(action);
-      return {
-        ...state,
-        isLogin: true,
-        user: {
-          ...state.user,
-          token: action.payload.id,
-          token2: action.payload.token2,
-          role: action.payload.role,
-          photo: action.payload.photo,
-          employee_id: action.payload.employee_id,
-          f_name:
-            action.payload.f_name == undefined ? "" : action.payload.f_name,
-          l_name:
-            action.payload.l_name == undefined ? "" : action.payload.l_name,
-        },
-      };
-    }
-    case "HANDLE_LOGOUT": {
-      return {
-        ...state,
-        isLogin: false,
-        user: {
-          ...state.user,
-          token: "",
-          token2: "",
-          photo: "",
-          role: "",
-          f_name: "",
-          l_name: "",
-          employee_id: "",
-        },
-      };
-    }
-    case "UPDATE_TOKEN": {
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          token: action.payload.id,
-        },
-      };
-    }
-    case "SAVE_ACTIVE_LINE": {
-      return {
-        ...state,
-        LineLogin: {
-          ...state.LineLogin,
-          line_id: action.payload.line_id,
-          line_name: action.payload.line_name,
-          line_login_time: action.payload.line_login_time,
-          type: action.payload.type,
-          isLogedIn: true,
-        },
-      };
-    }
-    case "LOGOUT_ACTIVE_LINE": {
-      return {
-        ...state,
-        LineLogin: {
-          ...state.LineLogin,
-          line_name: "",
-          line_login_time: "",
-          type: "",
-          isLogedIn: false,
-          line_id: "",
-          active_batch: "",
-        },
-      };
-    }
-    case "ACTIVE_BATCH": {
-      return {
-        ...state,
-        LineLogin: {
-          ...state.LineLogin,
-          active_batch: action.payload.name,
-        },
-      };
-    }
+const persistedReducer = persistReducer(persistConfig, Reducer);
 
-    default:
-      return state;
-  }
-};
+export let Store = configureStore({
+  reducer: { persistedReducer },
+});
+
+export let persistor = persistStore(Store);
